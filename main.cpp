40abcddc287807eec3b0820934fe4ad57bfc6a37
afterburner <rockey1024@icloud.com>
2019-01-25 16:22:35 +0800

    Before cropping

--- main.cpp
+++ main.cpp
@@ -29,6 +29,7 @@ int main(const int argc, const char *argv[])
     }
     UMat cl_src = src.getUMat(ACCESS_READ), cl_gray;
     cvtColor(cl_src, cl_gray, COLOR_BGR2GRAY);
+    equalizeHist(cl_gray, cl_gray);
 
     std::vector<std::pair<Rect2i, UMat>> candidates = getSubregionCandidates(cl_gray);
 //    show(candidates);
@@ -57,24 +58,60 @@ std::vector<RotatedRect> filter_rotatedRects_bySize(const std::vector<RotatedRec
         float ar = r.size.width / r.size.height;
         if(ar < 1.f) ar = 1.f / ar;
         const float minLength = min(r.size.width, r.size.height);
-        if(ar>low_AR and ar<high_AR and minLength > 15 and minLength < 100) ret.push_back(r);
+        if(ar>low_AR and ar<high_AR and minLength > 15 and minLength < 50) ret.push_back(r);
     }
     return ret;
 }
 
 
-UMat get_upRight_image(InputArray image, const RotatedRect &rt) {
-    float angle;
-    if (rt.size.width > rt.size.height) {
-        angle = rt.angle;
-    }else{
-        angle = rt.angle + 90.f;
+std::vector<UMat> get_upright_plates(InputArray image, const std::vector<RotatedRect>& rts) {
+    Mat bw;
+    std::vector<UMat> ret;
+    ret.reserve(rts.size());
+    for (const auto &rt: rts) {
+        float angle;
+        int height;
+        if (rt.size.width > rt.size.height) {
+            angle = rt.angle;
+            height = static_cast<int>(rt.size.height);
+        }else{
+            angle = rt.angle + 90.f;
+            height = static_cast<int>(rt.size.width);
+        }
+        Mat rotationMatrix = getRotationMatrix2D(rt.center, angle, 1);
+        UMat rotationRetified;
+        warpAffine(image, rotationRetified, rotationMatrix, image.size());
+        threshold(rotationRetified, bw, 100, 255, THRESH_BINARY);
+        imshow("bw", bw);
+        waitKey(0);
+        const int interval = height / 3;
+        const int y = static_cast<const int>(rt.center.y);
+        const int x = static_cast<const int>(rt.center.x);
+        std::vector<Point2i> seeds;
+        seeds.reserve(8);
+        for (int i = 1; i < 5; i++) {
+            seeds.emplace_back(x - i * interval, y);
+            seeds.emplace_back(x + i * interval, y);
+        }
+        UMat mask= UMat::zeros(rotationRetified.rows+2, rotationRetified.cols+2, CV_8UC1);
+        for (const auto &s : seeds) {
+                floodFill(bw, mask, s, {}, nullptr, 5, 5,
+                          4 | (255 << 8) | FLOODFILL_MASK_ONLY);
+        }
+        UMat mask_fitted = UMat(mask, Rect2i{1,1,rotationRetified.cols, rotationRetified.rows});
+//    threshold(mask_fitted, mask_fitted, 100, 255, THRESH_BINARY);
+        line(mask_fitted, seeds[6], seeds[7], 0, 5);
+        imshow("mask_fitted", mask_fitted);
+        waitKey(0);
+        //    std::vector<std::vector<Point2i>> contours;
+//    findContours(mask_fitted, contours, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
+
+        ret.emplace_back(rotationRetified, boundingRect(mask_fitted));
     }
 
-    Mat rotationMatrix = getRotationMatrix2D(rt.center, angle, 1);
-    UMat ret;
-    warpAffine(image, ret, rotationMatrix, {});
+
     return ret;
+
 }
 
 
@@ -88,18 +125,15 @@ std::vector<std::pair<Rect2i, UMat>> getSubregionCandidates(InputArray gray) {
     std::vector<std::vector<Point2i>> allContours;
     findContours(edgeImage, allContours, {}, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
     std::vector<RotatedRect> all_rotatedRects = get_rotatedRects(allContours);
-    std::vector<RotatedRect> rotatedRects_sizeQualified = filter_rotatedRects_bySize(all_rotatedRects); //TODO: Return noopt if size = 0
+    std::vector<RotatedRect> rotatedRects_sizeQualified = filter_rotatedRects_bySize(all_rotatedRects); //TODO: Return noopt if size = 0, ret.first = rt.boundingBox
 
-    std::vector<std::pair<Rect2i, UMat>> upRightRects_AND_rotationRectifiedImages;
-    upRightRects_AND_rotationRectifiedImages.reserve(rotatedRects_sizeQualified.size());
-    for (const auto &rt : rotatedRects_sizeQualified) {
-        upRightRects_AND_rotationRectifiedImages.emplace_back(rt.boundingRect(), get_upRight_image(blurred, rt));
-    }
-
-    for (const auto &p : upRightRects_AND_rotationRectifiedImages) {
-        imshow("debug", p.second);
+    std::cout << rotatedRects_sizeQualified.size() << '\n';
+    std::vector<UMat> upright_plates = get_upright_plates(blurred, rotatedRects_sizeQualified);
+    for (const auto &img : upright_plates) {
+        imshow("debug", img);
         waitKey(0);
     }
+
 
 
     return std::vector<std::pair<Rect2i, UMat>>();
